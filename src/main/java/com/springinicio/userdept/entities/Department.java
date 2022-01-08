f453bdd36da5699d399745c6965b4239297633c9
Eliseu <eliseusantico@gmail.com>
2022-01-08 20:53:13 -0300

    Link com o banco de dados criado com sucesso Banco H2

--- src/main/java/com/springinicio/userdept/entities/Department.java
+++ src/main/java/com/springinicio/userdept/entities/Department.java
@@ -1,5 +1,40 @@
 package com.springinicio.userdept.entities;
 
-public class Department {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "tb_department") 
+public class Department {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	public Department() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
+
+
